149bb614cd6ef94012e3358b44ad4b434fc17275
Dayna Cole <daynerco@gmail.com>
2024-04-02 22:03:35 -0400

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -30,3 +30,37 @@ function init(){
     makeDemographics(first_entry);
     }); //end d3 access
 };
+
+//function to populate the horizontal barchart
+function makeBar(sample){
+
+    //get sample data in order to populate bar chart
+    d3.json(url).then((data) => {
+        let sample_data = data.samples;
+        //align filter that matches sample id
+        let results = sample.data.filter(id => id.id ==sample);
+        // store first entry in results filter
+        let first_result = results[0];
+        console.log(first_result);
+        //store first 10 results to display in bar chart
+        let sample_values = first_result.sample_values.slice(0,10);
+        let otu_ids = first_result.otu_ids.slice(0.10);
+        let otu_labels = first_result.otu_labels.slice(0.10);
+        console.log(sample_values);
+        console.log(otu_ids);
+        console.log(otu_labels);
+
+        //create trace for bar chart
+        let bar_trace = {
+            x: sample_values.reverse(),
+            y: otu_ids.map(item => `OTU ${item}`).reverse(),
+            text: otu_labels.reverse(),
+            type: 'bar',
+            orientation: 'h'
+        };
+
+        let layout = {title: "Top 10 OTUs"};
+        Plotly.newPlot("bar", [bar_trace], layout);
+    });
+};
+
